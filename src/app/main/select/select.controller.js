bc26440c772a518a31e56300cbed7511632080af
Unknown <alexandre.asto63@orange.fr>
2018-03-10 17:39:31 +0100

    SELECT Controller

--- src/app/main/select/select.controller.js
+++ src/app/main/select/select.controller.js
@@ -7,9 +7,29 @@
         .controller('SelectController', SelectController);
 
     /** @ngInject */
-    function SelectController()
+    function SelectController($localStorage, $location)
     {
         var vm = this;
+        vm.selectProject = selectProject;
+
+
+
+
+        function selectProject(project) {
+            switch(project){
+                case 'Aquaponie':
+                    $localStorage.project = "Aquaponie";
+                    $location.path('/aquaponie');
+                    break;
+                case 'Piscine':
+                    $localStorage.project = "Piscine";
+                    $location.path('/piscine');
+                    break;
+                default:
+                    $localStorage.project = "Aquaponie";
+                    $location.path('/aquaponie');
+            }
+        }
 
     }
 })();
