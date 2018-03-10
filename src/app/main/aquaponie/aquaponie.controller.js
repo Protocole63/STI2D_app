30d8b21ba6c6184595f5906f615f1c020f1315e4
Unknown <alexandre.asto63@orange.fr>
2018-03-10 18:20:56 +0100

    Edit Aquaponie Controller

--- src/app/main/aquaponie/aquaponie.controller.js
+++ src/app/main/aquaponie/aquaponie.controller.js
@@ -7,12 +7,19 @@
         .controller('AquaponieController', AquaponieController);
 
     /** @ngInject */
-    function AquaponieController($localStorage, $location)
+    function AquaponieController($localStorage, $location, api, lastTemp, lastpH, lastTank)
     {
         var vm = this;
+        vm.lastTank = lastTank.feeds[0].field3;
         if(!$localStorage.project || $localStorage.project !== "Aquaponie") { $location.path('/select'); }
-        
-        vm.bulletChart = {
+        api.getLastTemp.aquaponie.get({},
+            function(response) {
+                return vm.lastTemp = response.feeds[0].field1;
+            },
+            function(response) {
+                console.log(response);
+        });
+        vm.temp = {
             options: {
                 chart: {
                     type              : 'bulletChart',
@@ -23,9 +30,23 @@
                 'title'   : 'Temperature',
                 'subtitle': '°C',
                 'ranges'  : [18, 22, 24],
-                'measures': [20]
+                'measures': [parseInt(lastTemp.feeds[0].field1)]
             }
         };
 
+        vm.ph = {
+            options: {
+                chart: {
+                    type              : 'bulletChart',
+                    transitionDuration: 500
+                }
+            },
+            data   : {
+                'title'   : 'Acidité Eau',
+                'subtitle': 'pH',
+                'ranges'  : [7, 7, 8],
+                'measures': [parseInt(lastpH.feeds[0].field2)]
+            }
+        };
     }
 })();
